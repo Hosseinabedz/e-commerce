5a364ec70a439859dca55a480ef69b552b0d12ac
HosseinAbedZ <hossein.abedz3937@gmail.com>
2023-11-23 15:25:26 +0330

    Add project files.

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+
+        public ProductController(StoreDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "single product";
+        }
+    }
+}
