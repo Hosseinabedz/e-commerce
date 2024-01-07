9601a461624c06b3eef5a7fb84bd4c28f364239b
HosseinAbedZ <hossein.abedz3937@gmail.com>
2024-01-07 14:21:18 +0330

    implement generic repo with specification methods

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -30,5 +31,21 @@ namespace Infrastructure.Data
         {
             return await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
         }
+
+        public async Task<T?> GetEntitySpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+        }
+
     }
 }
