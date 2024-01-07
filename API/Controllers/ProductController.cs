e9fbeae32c54447c8adce74d0d39205c3a689007
HosseinAbedZ <hossein.abedz3937@gmail.com>
2024-01-07 16:56:54 +0330

    create productToReturnDTO and implement in product controller

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Core.Interfaces;
 using Core.Specifications;
+using API.DTOs;
 
 namespace API.Controllers
 {
@@ -26,22 +27,43 @@ namespace API.Controllers
         #endregion
         
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<ProductToReturnDTO>>> GetProducts()
         {
             var spec = new ProductsWithTypesAndBrandsSpecification();
 
             var products = await _productRepo.ListAsync(spec);
 
-            return Ok(products);
+            // map to DTO
+            return products.Select(product => new ProductToReturnDTO
+            {
+                id = product.Id,
+                Description = product.Description,
+                Name = product.Name,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand.Name,
+                productType = product.productType.Name
+            }).ToList(); 
         }
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductToReturnDTO>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product =  await _productRepo.GetEntitySpec(spec);
 
-            return Ok(product);
+            var productToReturnDTO = new ProductToReturnDTO()
+            {
+                id = product.Id,
+                Description = product.Description,
+                Name = product.Name,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand.Name,
+                productType = product.productType.Name
+            };
+
+            return Ok(productToReturnDTO);
         }
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
